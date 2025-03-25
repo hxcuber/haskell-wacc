b06b7815c4bb23e8538e9b73efaeed3d7032884e
Hoang Nguyen <hn622@ic.ac.uk>
2025-03-25 22:48:28 +0000

    overloaded strings work

--- app/frontend/Lexer.hs
+++ app/frontend/Lexer.hs
@@ -1,153 +1,162 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Lexer where
-import Text.Gigaparsec.Token.Descriptions (plain, LexicalDesc (nameDesc, symbolDesc, textDesc, spaceDesc), plainName, NameDesc (..), plainSymbol, SymbolDesc (..), plainText, TextDesc (escapeSequences, graphicCharacter), plainEscape, EscapeDesc (..), plainSpace, SpaceDesc (lineCommentStart))
-import Data.Char (isLetter, isDigit, ord)
-import Text.Gigaparsec.Token.Lexer (mkLexerWithErrorConfig, Lexer (lexeme), decimal32, Names (identifier), Lexeme (names, stringLiteral, charLiteral), integer, TextParsers (ascii), apply, fully)
-import Text.Gigaparsec.Token.Errors (defaultErrorConfig, ErrorConfig (labelSymbol), reason)
-import qualified Text.Gigaparsec.Token.Lexer as Text.Gigaparsec.Token
-import qualified Text.Gigaparsec
-import Text.Gigaparsec (atomic, notFollowedBy)
+
+import Data.Char (isDigit, isLetter, ord)
+import Text.Gigaparsec (Parsec, atomic, notFollowedBy)
 import Text.Gigaparsec.Char (char, digit)
-import Data.String (IsString (fromString))
+import Text.Gigaparsec.Token.Descriptions (EscapeDesc (..), LexicalDesc (..), NameDesc (..), SpaceDesc (..), SymbolDesc (..), TextDesc (..), plain, plainEscape, plainName, plainSpace, plainSymbol, plainText)
+import Text.Gigaparsec.Token.Errors (ErrorConfig (..), ExplainConfigurable (reason), defaultErrorConfig)
+import Text.Gigaparsec.Token.Lexer (Lexeme (..), Lexer, ascii, decimal32, fully, identifier, lexeme, mkLexerWithErrorConfig, names)
+import Text.Gigaparsec.Token.Patterns (overloadedStrings)
 
-myLexicalDesc :: Text.Gigaparsec.Token.Descriptions.LexicalDesc
-myLexicalDesc = plain
-  {
-    nameDesc = myNameDesc,
-    symbolDesc = mySymbolDesc,
-    textDesc = myTextDesc,
-    spaceDesc = mySpaceDesc
-  }
+myLexicalDesc :: LexicalDesc
+myLexicalDesc =
+  plain
+    { nameDesc = myNameDesc,
+      symbolDesc = mySymbolDesc,
+      textDesc = myTextDesc,
+      spaceDesc = mySpaceDesc
+    }
 
-myNameDesc :: Text.Gigaparsec.Token.Descriptions.NameDesc
-myNameDesc = plainName
-  { identifierStart = Just (\c -> isLetter c || c == '_'),
-    identifierLetter = Just (\c -> isLetter c || isDigit c || c == '_')
-  }
+myNameDesc :: NameDesc
+myNameDesc =
+  plainName
+    { identifierStart = Just (\c -> isLetter c || c == '_'),
+      identifierLetter = Just (\c -> isLetter c || isDigit c || c == '_')
+    }
 
-mySymbolDesc :: Text.Gigaparsec.Token.Descriptions.SymbolDesc
-mySymbolDesc = plainSymbol
-  {
-    hardKeywords =
-      [ "true",
-        "false",
-        "null",
-        "int",
-        "bool",
-        "char",
-        "string",
-        "pair",
-        "begin",
-        "end",
-        "is",
-        "skip",
-        "read",
-        "free",
-        "return",
-        "exit",
-        "print",
-        "println",
-        "if",
-        "then",
-        "else",
-        "fi",
-        "while",
-        "do",
-        "done",
-        "newpair",
-        "call",
-        "len",
-        "ord",
-        "chr",
-        "fst",
-        "snd"
-      ],
-    hardOperators =
-      [ "!",
-        "-",
-        "*",
-        "/",
-        "%",
-        "+",
-        "-",
-        "<",
-        "<=",
-        ">",
-        ">=",
-        "==",
-        "!=",
-        "&&",
-        "||",
-        "="
-      ]
-  }
+mySymbolDesc :: SymbolDesc
+mySymbolDesc =
+  plainSymbol
+    { hardKeywords =
+        [ "true",
+          "false",
+          "null",
+          "int",
+          "bool",
+          "char",
+          "string",
+          "pair",
+          "begin",
+          "end",
+          "is",
+          "skip",
+          "read",
+          "free",
+          "return",
+          "exit",
+          "print",
+          "println",
+          "if",
+          "then",
+          "else",
+          "fi",
+          "while",
+          "do",
+          "done",
+          "newpair",
+          "call",
+          "len",
+          "ord",
+          "chr",
+          "fst",
+          "snd"
+        ],
+      hardOperators =
+        [ "!",
+          "-",
+          "*",
+          "/",
+          "%",
+          "+",
+          "-",
+          "<",
+          "<=",
+          ">",
+          ">=",
+          "==",
+          "!=",
+          "&&",
+          "||",
+          "="
+        ]
+    }
 
-myTextDesc :: Text.Gigaparsec.Token.Descriptions.TextDesc
-myTextDesc = plainText
-  {
-    escapeSequences = plainEscape
-      {
-        literals = ['\\', '"', '\''],
-        mapping  =
-          [ ("0", '\0'),
-            ("b", '\b'),
-            ("t", '\t'),
-            ("n", '\n'),
-            ("f", '\f'),
-            ("r", '\r')
-          ]
-      },
-    graphicCharacter = Just (\c -> c /= '"' && c /= '\\' && c /= '\'' && ord c >= 0x20)
-  }
+myTextDesc :: TextDesc
+myTextDesc =
+  plainText
+    { escapeSequences =
+        plainEscape
+          { literals = ['\\', '"', '\''],
+            mapping =
+              [ ("0", '\0'),
+                ("b", '\b'),
+                ("t", '\t'),
+                ("n", '\n'),
+                ("f", '\f'),
+                ("r", '\r')
+              ]
+          },
+      graphicCharacter = Just (\c -> c /= '"' && c /= '\\' && c /= '\'' && ord c >= 0x20)
+    }
 
-mySpaceDesc :: Text.Gigaparsec.Token.Descriptions.SpaceDesc
-mySpaceDesc = plainSpace
-  { lineCommentStart = "#"
-  }
+mySpaceDesc :: SpaceDesc
+mySpaceDesc =
+  plainSpace
+    { lineCommentStart = "#"
+    }
 
-myErrorConfig :: Text.Gigaparsec.Token.Errors.ErrorConfig
-myErrorConfig = defaultErrorConfig
-  {
-    labelSymbol =
-      [ ("!", reason "unary operator"),
-        ("*", reason "arithmetic operator"),
-        ("/", reason "arithmetic operator"),
-        ("%", reason "arithmetic operator"),
-        ("+", reason "arithmetic operator"),
-        ("-", reason "arithmetic operator"),
-        ("<", reason "boolean operator"),
-        ("<=", reason "boolean operator"),
-        (">", reason "boolean operator"),
-        (">=", reason "boolean operator"),
-        ("==", reason "boolean operator"),
-        ("!=", reason "boolean operator"),
-        ("&&", reason "boolean operator"),
-        ("||", reason "boolean operator"),
-        ("=", reason "arithmetic operator")
-      ]
-  }
+myErrorConfig :: ErrorConfig
+myErrorConfig =
+  defaultErrorConfig
+    { labelSymbol =
+        [ ("!", reason "unary operator"),
+          ("*", reason "arithmetic operator"),
+          ("/", reason "arithmetic operator"),
+          ("%", reason "arithmetic operator"),
+          ("+", reason "arithmetic operator"),
+          ("-", reason "arithmetic operator"),
+          ("<", reason "boolean operator"),
+          ("<=", reason "boolean operator"),
+          (">", reason "boolean operator"),
+          (">=", reason "boolean operator"),
+          ("==", reason "boolean operator"),
+          ("!=", reason "boolean operator"),
+          ("&&", reason "boolean operator"),
+          ("||", reason "boolean operator"),
+          ("=", reason "arithmetic operator")
+        ]
+    }
 
-myLexer :: Text.Gigaparsec.Token.Lexer.Lexer
+myLexer :: Lexer
 myLexer = mkLexerWithErrorConfig myLexicalDesc myErrorConfig
 
-myLexeme :: Text.Gigaparsec.Token.Lexeme
+myLexeme :: Lexeme
 myLexeme = lexeme myLexer
 
-myIdentifier :: Text.Gigaparsec.Parsec String
+myIdentifier :: Parsec String
 myIdentifier = identifier $ names myLexeme
 
-myInteger :: Text.Gigaparsec.Parsec Int
+myInteger :: Parsec Int
 myInteger = decimal32 $ integer myLexeme
 
-myString :: Text.Gigaparsec.Parsec String
+myString :: Parsec String
 myString = ascii $ stringLiteral myLexeme
 
-myChar :: Text.Gigaparsec.Parsec Char
+myChar :: Parsec Char
 myChar = ascii $ charLiteral myLexeme
 
-negUnary :: Text.Gigaparsec.Parsec ()
+negUnary :: Parsec ()
 negUnary = apply myLexeme (atomic (char '-' *> notFollowedBy digit))
 
-myFully :: Text.Gigaparsec.Parsec a -> Text.Gigaparsec.Parsec a
+myFully :: Parsec a -> Parsec a
 myFully = fully myLexer
+
+symLexeme :: String -> Parsec ()
+symLexeme = sym myLexeme
+
+overloadedStrings [|myLexer|]
--- app/frontend/TypeParser.hs
+++ app/frontend/TypeParser.hs
@@ -1,40 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module TypeParser where
-import AST (BaseType (BInt, BBool, BChar, BString), Type (..), ArrayType (ArrayType), PairType (PairType), PairElemType (BasePE, ArrayPE, PairPE))
-import Text.Gigaparsec.Char (string)
-import Text.Gigaparsec (($>), (<|>), Parsec, (<~>))
-import Text.Gigaparsec.Combinator (count, option, count1)
+
+import AST (ArrayType (ArrayType), BaseType (BBool, BChar, BInt, BString), PairElemType (ArrayPE, BasePE, PairPE), PairType (PairType), Type (..))
+import Text.Gigaparsec (Parsec, ($>), (<|>), (<~>))
+import Text.Gigaparsec.Combinator (count, count1, option)
+import Lexer ()
 
 typeParser :: Parsec Type
 typeParser = typeOrArrayType ((BaseT <$> baseType) <|> (PairT <$> pairType))
 
 baseType :: Parsec BaseType
-baseType = string "int"    $> BInt    <|>
-           string "bool"   $> BBool   <|>
-           string "char"   $> BChar   <|>
-           string "string" $> BString
+baseType = "int"    $> BInt    <|>
+           "bool"   $> BBool   <|>
+           "char"   $> BChar   <|>
+           "string" $> BString
 
 typeOrArrayType :: Parsec Type -> Parsec Type
 typeOrArrayType pt =
-  disambiguator <$> (pt <~> count (string "[" *> string "]"))
-    where
-      disambiguator (t, d) = if d == 0 then t else ArrayT (ArrayType t d)
+  disambiguator <$> (pt <~> count ("[" *> "]"))
+  where
+    disambiguator (t, d) = if d == 0 then t else ArrayT (ArrayType t d)
 
 pairType :: Parsec PairType
 pairType =
-  disambiguator <$> ((string "pair" *> string "(" *> pairElemType) <~> (string "," *> pairElemType <* string ")"))
-    where
-      disambiguator (pet1, pet2) = PairType pet1 pet2
+  disambiguator <$> (("pair" *> "(" *> pairElemType) <~> ("," *> pairElemType <* ")"))
+  where
+    disambiguator (pet1, pet2) = PairType pet1 pet2
 
 pairElemType :: Parsec PairElemType
-pairElemType = (helper1 <$> typeOrArrayType (BaseT <$> baseType)) <|>
-               (helper2 <$> (string "pair" *> option ((string "(" *> pairElemType <* string ",") <~> (pairElemType <* string ")") <~> count1 (string "[" *> string "]"))))
+pairElemType =
+  (helper1 <$> typeOrArrayType (BaseT <$> baseType))
+    <|> (helper2 <$> ("pair" *> option (("(" *> pairElemType <* ",") <~> (pairElemType <* ")") <~> count1 ("[" *> "]"))))
   where
     helper1 (BaseT bt) = BasePE bt
     helper1 (ArrayT at) = ArrayPE at
     -- never hit
-    helper1 (PairT _)   = PairPE
+    helper1 (PairT _) = PairPE
 
-    helper2 Nothing                  = PairPE
+    helper2 Nothing = PairPE
     helper2 (Just ((pet1, pet2), d)) = ArrayPE (ArrayType (PairT (PairType pet1 pet2)) d)
-
-
